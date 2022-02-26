e1f9108b06efec6780dc647510631e1f95ea7e71
Yoevelyn Rodriguez <yoerodri@yahoo.com>
2022-02-26 16:42:17 -0500

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,14 @@
+
+/*************************************************************************
+	 * CSCI 185 M07 Spring 2022
+	 * Patrick Liang
+	 * Omar Hu
+	 * Ken Xijian Feng Xun
+	 * Yoevelyn Rodriguez
+	 * M3: Homework #2 Inheritance and Composition
+	 * 3/08/2022
+	 ************************************************************************/
+
 public class Teacher extends Person{
     private String ID;
     //I cannot use # in variable name
