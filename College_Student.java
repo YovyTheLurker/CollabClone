15bad1180e0c07108d24f1edee24a45d2e38c1cb
Yoevelyn Rodriguez <yoerodri@yahoo.com>
2022-02-26 16:46:00 -0500

    Update College_Student.java

--- College_Student.java
+++ College_Student.java
@@ -1,3 +1,13 @@
+/*************************************************************************
+	 * CSCI 185 M07 Spring 2022
+	 * Patrick Liang
+	 * Omar Hu
+	 * Ken Xijian Feng Xun
+	 * Yoevelyn Rodriguez
+	 * M3: Homework #2 Inheritance and Composition
+	 * 3/08/2022
+	 ************************************************************************/
+
 public class College_Student extends Student{
     // up to you ken if you wanna keep these public or make them private
     
