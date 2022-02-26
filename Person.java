717533d02cad0d16cab80486f8ffc0c5346ed303
Yoevelyn Rodriguez <yoerodri@yahoo.com>
2022-02-26 16:45:23 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,13 @@
+/*************************************************************************
+	 * CSCI 185 M07 Spring 2022
+	 * Patrick Liang
+	 * Omar Hu
+	 * Ken Xijian Feng Xun
+	 * Yoevelyn Rodriguez
+	 * M3: Homework #2 Inheritance and Composition
+	 * 3/08/2022
+	 ************************************************************************/
+
 public class Person{
     private String name;
     private boolean alive;
